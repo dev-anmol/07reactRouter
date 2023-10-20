3fc22435373b3238c2cabf07c8c9543de745f35f
Anmol Thakur <anmolll.thakurrr@gmail.com>
2023-10-20 15:48:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default App;
